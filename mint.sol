dacf321447045fe084c772156d58ce8fd1d55f67
Jaimmy-hub <137689315+Jaimmy-hub@users.noreply.github.com>
2024-05-06 20:42:37 +0800

    Update mint.sol

--- mint.sol
+++ mint.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract Mytoken is ERC20 {
+    constructor() ERC20("MyToken","MTK") {
+        _mint(msg.sender, 100 * 10**uint(decimals()));
+    }
+
+}
